7b712cb252aa9b299ee71318464f0767b64da78c
WillGitDev <w.grondin@orange.fr>
2026-03-30 11:35:44 +0400

    premier commit

---
+++ prisma/seed.js
@@ -0,0 +1,23 @@
+import { prisma } from "../lib/prisma.js";
+import photographers from "../data/photographer.json" with { type: "json" };
+import medias from "../data/media.json" with { type: "json" };
+
+async function main() {
+  await prisma.photographer.createMany({
+    data: photographers,
+  });
+
+  await prisma.media.createMany({
+    data: medias, // content from ./data/media.json
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
